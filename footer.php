846ed7a5500f6b0a13fac51bc83425285a7fabe2
Tob <surachartsomma@gmail.com>
2018-08-30 14:53:30 +0700

    Update footer

--- footer.php
+++ footer.php
@@ -49,7 +49,7 @@
 
 		<div class="footer-sponsors">
 			<p>
-				<a href="#" style="width: 180px;"><img src="<?php bloginfo('stylesheet_directory');?>/images/mpe-logo-colorized.png" alt="Mekong Partnership for the Environment"></a>
+				<a href="#" style="width: 180px;"><img src="<?php bloginfo('stylesheet_directory');?>/images/SIDA-Logo.png" alt="Sweden Sveriage"></a>
 				<a href="http://earthjournalism.net" style="width: 150px;"><img src="<?php bloginfo('stylesheet_directory');?>/images/ejn-logo-hi.png" alt="Earth Journalism Network"></a>
 				<a href="https://www.internews.org" style="width: 180px;"><img src="<?php bloginfo('stylesheet_directory');?>/images/internews-logo-colorized.png" alt="Internews"></a>
 			</p>
@@ -58,8 +58,8 @@
 		<div class="footer-contact">
 			<p>
 				<a href="mailto:editor@mekongeye.com">editor@mekongeye.com</a><br>
-				Unit 6, 28th Floor, CRC Tower,&nbsp;All Seasons Place<br>
-				87/2 Wireless Road, Pathumwan<br>
+				Address 518/5 Maneeya Center Bldg., 10th Flr.,<br>
+				Ploenchit Rd., Pathumwan,<br>
 				Bangkok, 10330<br>
 				Thailand
 			</p>

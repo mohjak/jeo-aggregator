ed687c6290cb8d2183f394a31374b7ad01d52aa4
Tob <surachartsomma@gmail.com>
2018-11-05 17:36:38 +0700

    Add tracking for post templates

--- loop.php
+++ loop.php
@@ -3,7 +3,15 @@
 		<li id="post-<?php the_ID(); ?>" <?php post_class('post-item four columns'); ?>>
 			<article>
 				<header class="post-header">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+					<?php 
+					$data_set_post = get_post_meta( get_the_ID(), 'dataset_content', true);
+					if ($data_set_post == '1') {
+						$tracking = 'onclick="trackOutboundLink('. get_the_permalink() .');';  
+					}else {
+						$tracking = '';
+					}
+					?>
+					<a href="<?php the_permalink(); ?>" <?php echo $tracking; ?> title="<?php the_title(); ?>">
 						<?php
 						if(has_post_thumbnail())
 							the_post_thumbnail(array(800, 650));
@@ -11,7 +19,7 @@
 							echo '<img width="800" hight="650" src="' . get_post_meta($post->ID, 'picture', true) . '" />';
 						?>
 					</a>
-					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<h2><a href="<?php the_permalink(); ?>" <?php echo $tracking; ?>><?php the_title(); ?></a></h2>
 					<p class="meta clearfix">
 						<span class="date">
 							<span class="lsf">&#xE15e;</span>
